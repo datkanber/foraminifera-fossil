3fe5bbb9362c2720ef07cb8750899f760f5f2fcb
datkanber <burak2kanber@gmail.com>
2026-07-23 19:47:59 +0300

    Fix Babel production jsxDEV issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
+if (process.argv.includes("production")) {
+  process.env.NODE_ENV = "production";
+}
+
 module.exports = {
   entry: "./src/index.js",
 
